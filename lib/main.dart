a1d9bdca90a535478b5715af27a9ad9d90372eb3
OsamaxD1 <hassanosama16b@gmail.com>
2020-04-20 16:58:38 +0500

    Back front flippable screen

--- lib/main.dart
+++ lib/main.dart
@@ -24,20 +24,19 @@ class _FlippableExampleState extends State<FlippableExample>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.red.shade300,
-        body: Center(
-
-            child: GestureDetector(
-              onTap: () => setState(() => _isFlipped = !_isFlipped),
-              child: FlippableBox(
-                front: _buildFront(),
-                back: _buildBack("Back...", 350, 700, Colors.white),
-                flipVt: false,
-                isFlipped: _isFlipped,
-              ),
-            ),
+      backgroundColor: Colors.red.shade300,
+      body: Center(
+        child: GestureDetector(
+          onTap: () => setState(() => _isFlipped = !_isFlipped),
+          child: FlippableBox(
+            front: _buildFront(),
+            back: _buildBack(context),
+            flipVt: false,
+            isFlipped: _isFlipped,
           ),
-        );
+        ),
+      ),
+    );
   }
 
   Widget _buildFront() {
@@ -70,13 +69,14 @@ class _FlippableExampleState extends State<FlippableExample>
                   ),
                   textAlign: TextAlign.left,
                 )),
-
-               Align(
-                alignment: Alignment.bottomCenter,
-                child: Image.asset('images/rickandmorty.png', height: 300.0,
-                  width: 250.0,),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset(
+                'images/rickandmorty.png',
+                height: 300.0,
+                width: 250.0,
               ),
-
+            ),
           ],
         ),
       ),
@@ -84,13 +84,261 @@ class _FlippableExampleState extends State<FlippableExample>
   }
 }
 
-Widget _buildBack(String label, double width, double height, Color color) {
+Widget _buildBack(context) {
   return Container(
-    color: color,
-    width: width,
-    height: height,
-    child: Center(
-      child: Text(label, style: TextStyle(fontSize: 32)),
+    color: Colors.white,
+    width: 350,
+    height: 700,
+    child: Stack(
+      children: <Widget>[
+        Image.asset(
+          'images/rickandmortyeye.jpg',
+          fit: BoxFit.fill,
+        ),
+        Positioned(
+          top: 25,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    color: Color(0xff61688B),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 5,
+                    horizontal: 10,
+                  ),
+                  color: Color(0xffFFD073),
+                  child: Text(
+                    'New Episode',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: MediaQuery.of(context).size.height / 2 + 60,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(20),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      'Season 4',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        ListTile(
+                          contentPadding: EdgeInsets.all(0),
+                          leading: Text(
+                            "01",
+                            style: TextStyle(
+                              color: Color(0xffE4E7F4),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                          title: Text(
+                            "Edge of Tomorty",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          subtitle: Text("2.33k Views"),
+                          trailing: MaterialButton(
+                            onPressed: () {},
+                            color: Color(0xff182c47),
+                            padding: EdgeInsets.all(7),
+                            minWidth: 30,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Icon(
+                              Icons.arrow_right,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          contentPadding: EdgeInsets.all(0),
+                          leading: Text(
+                            "02",
+                            style: TextStyle(
+                              color: Color(0xffE4E7F4),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                          title: Text(
+                            "The Old Man and the Seat",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          subtitle: Text("2.32k Views"),
+                          trailing: MaterialButton(
+                            onPressed: () {},
+                            color: Color(0xff182c47),
+                            padding: EdgeInsets.all(7),
+                            minWidth: 30,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Icon(
+                              Icons.arrow_right,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          contentPadding: EdgeInsets.all(0),
+                          leading: Text(
+                            "03",
+                            style: TextStyle(
+                              color: Color(0xffE4E7F4),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                          title: Text(
+                            "One Crew",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          subtitle: Text("2.34k Views"),
+                          trailing: MaterialButton(
+                            onPressed: () {},
+                            color: Color(0xff182c47),
+                            padding: EdgeInsets.all(7),
+                            minWidth: 30,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Icon(
+                              Icons.arrow_right,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          contentPadding: EdgeInsets.all(0),
+                          leading: Text(
+                            "04",
+                            style: TextStyle(
+                              color: Color(0xffE4E7F4),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                          title: Text(
+                            "Claws and Hoarder",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          subtitle: Text("2.43k Views"),
+                          trailing: MaterialButton(
+                            onPressed: () {},
+                            color: Color(0xff182c47),
+                            padding: EdgeInsets.all(7),
+                            minWidth: 30,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Icon(
+                              Icons.arrow_right,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          contentPadding: EdgeInsets.all(0),
+                          leading: Text(
+                            "05",
+                            style: TextStyle(
+                              color: Color(0xffE4E7F4),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                          title: Text(
+                            "Rattlestar Ricklactica",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          subtitle: Text("2.43k Views"),
+                          trailing: MaterialButton(
+                            onPressed: () {},
+                            color: Color(0xff182c47),
+                            padding: EdgeInsets.all(7),
+                            minWidth: 30,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Icon(
+                              Icons.arrow_right,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+
+      ],
     ),
   );
 }
